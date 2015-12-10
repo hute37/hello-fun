7d0fa1f9cb55a6818f9885b456d0cc826f189ccf
Giovanni Pelosi <hute37@gmail.com>
2015-12-10 17:59:27 +0000

    codenvy main

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import inspect
 import os
+import sys
 
-os.chdir('code')
+X="adder.py"
 
-adder.py
 
+def get_script_dir(follow_symlinks=True):
+    if getattr(sys, 'frozen', False): # py2exe, PyInstaller, cx_Freeze
+        path = os.path.abspath(sys.executable)
+    else:
+        path = inspect.getabsfile(get_script_dir)
+    if follow_symlinks:
+        path = os.path.realpath(path)
+    return os.path.dirname(path)
+
+script_dir = get_script_dir()  
+print(script_dir)
+
+os.chdir(script_dir+'/code')
+
+with open(X,'r') as fh:
+  exec(fh.read())
